364426ea418971d63f484903ac94ab9b14b6bb8a
Christopher Shea <cmshea@gmail.com>
2019-08-16 14:54:30 -0400

    Deps output isn't interesting?

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-lein update-in :plugins conj "[lein-ancient \"0.6.15\"]" -- deps
+lein update-in :plugins conj "[lein-ancient \"0.6.15\"]" -- deps > /dev/null
 
 lein update-in :plugins conj "[lein-ancient \"0.6.15\"]" -- ancient
